05579b1187ecbbfbdec752ec494fece054093ea1
HARRIS KURNIADI SUMBOGO <harris.kurniadi@softorb.co.id>
2023-12-06 14:49:33 +0700

    add event card

---
+++ src/views/EventCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-card :to="{}">
+    <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px" cover> </v-img>
+
+    <v-card-item>
+      <v-card-title> Title </v-card-title>
+    </v-card-item>
+
+    <v-card-text>
+      <div class="d-flex align-end mb-3">
+        <v-icon class="me-1">{{ mdiCalendarBlank }}</v-icon>
+        02 January 2024
+      </div>
+
+      <div class="d-flex align-end">
+        <v-icon class="me-1">{{ mdiMapMarker }}</v-icon>
+        Yogyakarta
+      </div>
+    </v-card-text>
+
+    <v-divider class="mx-4"></v-divider>
+
+    <v-card-text>
+      <div>
+        <div class="text-caption">Mulai dari</div>
+      </div>
+
+      <div class="text-h6 font-weight-medium">Rp250.000</div>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+import { mdiCalendarBlank, mdiMapMarker } from '@mdi/js'
+</script>
+
+<style scoped></style>
