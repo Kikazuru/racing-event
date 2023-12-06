785fded7397ad6ca97dac359b86e117131d289a8
HARRIS KURNIADI SUMBOGO <harris.kurniadi@softorb.co.id>
2023-12-06 13:55:55 +0700

    add header

---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app-bar flat>
+    <div class="w-100 d-flex justify-center">
+      <div>LOGO</div>
+    </div>
+  </v-app-bar>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
