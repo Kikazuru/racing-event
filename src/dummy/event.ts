35a6f9510cd2c10b5934722ed35ae6c97b56d732
HARRIS KURNIADI SUMBOGO <harris.kurniadi@softorb.co.id>
2023-12-06 16:00:51 +0700

    fix data dummy

--- src/dummy/event.ts
+++ src/dummy/event.ts
@@ -36,7 +36,7 @@ const events: Event[] = [
     location: 'Yogyakarta'
   },
   {
-    id: '3',
+    id: '4',
     name: 'Lari 50k',
     imageUrl:
       'https://scontent.fjog3-1.fna.fbcdn.net/v/t39.30808-6/275587878_4904312262999038_7200719498784543191_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=783fdb&_nc_ohc=GeMZ_DOeVD4AX-kIkGk&_nc_ht=scontent.fjog3-1.fna&cb_e2o_trans=t&oh=00_AfDalOtVzl5gDpNQbXA5F6W1Gu_JpqN7pmGFhp8quEVfBA&oe=65742E7A',
@@ -45,7 +45,7 @@ const events: Event[] = [
     location: 'Yogyakarta'
   },
   {
-    id: '3',
+    id: '5',
     name: 'Lari 70k',
     imageUrl:
       'https://scontent.fjog3-1.fna.fbcdn.net/v/t39.30808-6/275587878_4904312262999038_7200719498784543191_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=783fdb&_nc_ohc=GeMZ_DOeVD4AX-kIkGk&_nc_ht=scontent.fjog3-1.fna&cb_e2o_trans=t&oh=00_AfDalOtVzl5gDpNQbXA5F6W1Gu_JpqN7pmGFhp8quEVfBA&oe=65742E7A',
@@ -54,12 +54,12 @@ const events: Event[] = [
     location: 'Yogyakarta'
   },
   {
-    id: '4',
+    id: '6',
     name: 'LLK - Lari Lari Kecil',
     imageUrl:
       'https://dyuarqvpljnfesefojys.supabase.co/storage/v1/object/public/baim/UGMTR%202023%20-%20BANNER%20PENDAFTARAN%20(1).jpg',
     price: 200_000,
-    date: '20 Januari 2024',
+    date: '1 Oktober 2023',
     location: 'Semarang'
   }
 ]
