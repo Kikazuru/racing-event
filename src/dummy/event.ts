271d79e3c5362f3c3d728b7b9f95059271fc746b
HARRIS KURNIADI SUMBOGO <harris.kurniadi@softorb.co.id>
2023-12-07 09:36:17 +0700

    adjust id with etix product

--- src/dummy/event.ts
+++ src/dummy/event.ts
@@ -9,52 +9,15 @@ interface Event {
 
 const events: Event[] = [
   {
-    id: '1',
-    name: 'Lari 5k',
-    imageUrl:
-      'https://scontent.fjog3-1.fna.fbcdn.net/v/t39.30808-6/275587878_4904312262999038_7200719498784543191_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=783fdb&_nc_ohc=GeMZ_DOeVD4AX-kIkGk&_nc_ht=scontent.fjog3-1.fna&cb_e2o_trans=t&oh=00_AfDalOtVzl5gDpNQbXA5F6W1Gu_JpqN7pmGFhp8quEVfBA&oe=65742E7A',
+    id: 'ctc-ultra-2024',
+    name: 'CTC Ultra 2024',
+    imageUrl: 'https://etix-storage.s3.ap-southeast-3.amazonaws.com/1701848762869image0.jpg',
     price: 275_000,
-    date: '11 Juni 2022',
+    date: '24 Februari 2024',
     location: 'Yogyakarta'
   },
   {
     id: '2',
-    name: 'Lari 13k',
-    imageUrl:
-      'https://scontent.fjog3-1.fna.fbcdn.net/v/t39.30808-6/275587878_4904312262999038_7200719498784543191_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=783fdb&_nc_ohc=GeMZ_DOeVD4AX-kIkGk&_nc_ht=scontent.fjog3-1.fna&cb_e2o_trans=t&oh=00_AfDalOtVzl5gDpNQbXA5F6W1Gu_JpqN7pmGFhp8quEVfBA&oe=65742E7A',
-    price: 425_000,
-    date: '11 Juni 2022',
-    location: 'Yogyakarta'
-  },
-  {
-    id: '3',
-    name: 'Lari 25k',
-    imageUrl:
-      'https://scontent.fjog3-1.fna.fbcdn.net/v/t39.30808-6/275587878_4904312262999038_7200719498784543191_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=783fdb&_nc_ohc=GeMZ_DOeVD4AX-kIkGk&_nc_ht=scontent.fjog3-1.fna&cb_e2o_trans=t&oh=00_AfDalOtVzl5gDpNQbXA5F6W1Gu_JpqN7pmGFhp8quEVfBA&oe=65742E7A',
-    price: 765_000,
-    date: '11 Juni 2022',
-    location: 'Yogyakarta'
-  },
-  {
-    id: '4',
-    name: 'Lari 50k',
-    imageUrl:
-      'https://scontent.fjog3-1.fna.fbcdn.net/v/t39.30808-6/275587878_4904312262999038_7200719498784543191_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=783fdb&_nc_ohc=GeMZ_DOeVD4AX-kIkGk&_nc_ht=scontent.fjog3-1.fna&cb_e2o_trans=t&oh=00_AfDalOtVzl5gDpNQbXA5F6W1Gu_JpqN7pmGFhp8quEVfBA&oe=65742E7A',
-    price: 945_000,
-    date: '12 Juni 2022',
-    location: 'Yogyakarta'
-  },
-  {
-    id: '5',
-    name: 'Lari 70k',
-    imageUrl:
-      'https://scontent.fjog3-1.fna.fbcdn.net/v/t39.30808-6/275587878_4904312262999038_7200719498784543191_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=783fdb&_nc_ohc=GeMZ_DOeVD4AX-kIkGk&_nc_ht=scontent.fjog3-1.fna&cb_e2o_trans=t&oh=00_AfDalOtVzl5gDpNQbXA5F6W1Gu_JpqN7pmGFhp8quEVfBA&oe=65742E7A',
-    price: 1_250_000,
-    date: '12 Juni 2022',
-    location: 'Yogyakarta'
-  },
-  {
-    id: '6',
     name: 'LLK - Lari Lari Kecil',
     imageUrl:
       'https://dyuarqvpljnfesefojys.supabase.co/storage/v1/object/public/baim/UGMTR%202023%20-%20BANNER%20PENDAFTARAN%20(1).jpg',
--- src/views/DetailEvent.vue
+++ src/views/DetailEvent.vue
@@ -47,7 +47,7 @@
           id="iframe-widget-etix"
           width="100%"
           scrolling="no"
-          src="http://etix-widget-dev.s3-website.ap-southeast-3.amazonaws.com/?color=2C3C3C&lan=id&product=ctc-ultra-2024&store=Sample Store 4"
+          :src="url"
           frameborder="0"
         ></iframe>
       </v-col>
@@ -66,6 +66,7 @@ const router = useRouter()
 const id = route.params.id as string
 
 const event = detailEvent(id)
+const url = `http://etix-widget-dev.s3-website.ap-southeast-3.amazonaws.com/?color=2C3C3C&lan=id&product=${id}&store=Sample Store 4`
 </script>
 
 <style scoped></style>
